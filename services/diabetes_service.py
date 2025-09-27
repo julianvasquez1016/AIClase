ac324cbb958b4c235a1c756295b6fcb26cc71644
Julian <julian@gmail.com>
2025-09-27 08:51:37 -0500

    start project

---
+++ services/diabetes_service.py
@@ -0,0 +1,29 @@
+import pickle
+
+import numpy as np
+from schemas.diabetes_schemas import PatientData
+
+with open('RFDiabetesv132.pkl','rb') as file:
+    model = pickle.load(file)
+
+labels = ["Sano", "Enfermo"]
+
+def diabetes_prediction(data: PatientData):
+
+    xin = np.array([
+        data.pregnancies,
+        data.glucose,
+        data.bloodpressure,
+        data.skinthickness,
+        data.insulin,
+        data.bmi,
+        data.diabetespedigreefunction,
+        data.age
+    ]).reshape(1, 8)
+
+
+    prediction = model.predict(xin)
+
+    print("prediction ", prediction)
+
+    return labels[prediction[0]]
